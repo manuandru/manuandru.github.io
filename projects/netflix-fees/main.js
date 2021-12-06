9a0350bdedfa39f98ab882767151f3e5d52ae46e
manuandru <manuandru987@gmail.com>
2021-12-06 14:28:11 +0100

    Fix edge case

--- projects/netflix-fees/main.js
+++ projects/netflix-fees/main.js
@@ -1,35 +1,31 @@
 const fees = [{
     name: 'Mala',
-    lastPayment: '2021-05-24',
+    payedUp: '2021-05-23',
     reminder: -2
 }, {
     name: 'Galva',
-    lastPayment: '2020-09-24',
+    payedUp: '2020-09-23',
     reminder: -25
 }, {
     name: 'Cecca',
-    lastPayment: '2021-11-24',
+    payedUp: '2021-11-23',
     reminder: 0
 }];
 
 const netflixPrice = 4.5;
+const billingDay = 24;
 
 Date.prototype.getMonthName = function() {
     const Months = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
 
-    return Months[this.getMonth()];
+    return Months[(this.getDate() >= billingDay ? this.getMonth() + 1 : this.getMonth()) % 12];
 }
 
-
 class User {
-    constructor(name, lastPaymentDate, reminder) {
+    constructor(name, payedUp, reminder) {
         this.name = name;
-        this.lastPaymentDate = new Date(lastPaymentDate);
+        this.payedUp = new Date(payedUp);
         this.reminder = reminder;
-    }
-
-    oneMonth() {
-        return 1000 * 3600 * 24 * 30;
     }
     
     /* return amount of money user have to pay */
@@ -39,30 +35,18 @@ class User {
 
     /* return number of missing Month */
     getNumberOfMissingMonth() {
-        const today = new Date();
-        const lastPayment = new Date(this.lastPaymentDate);
-        const diff = today.getTime() - lastPayment.getTime();
-        const monthDiff =
-            new Date().getDate() >= this.lastPaymentDate.getDate()
-            ? Math.ceil(diff / this.oneMonth())
-            : Math.floor(diff / this.oneMonth());
-        return monthDiff;
+        return this.getMissingMonths().length;
     }
 
     /* return an Array of missing Month */
     getMissingMonths() {
         const missingMonth = [];
-        /* the month decrement could be better cause
-        it's made by a statistic of month in 30 days
-        there's a bug if month >~ 30 */
-        let today = new Date(
-            new Date().getFullYear(),
-            new Date().getDate() > this.lastPaymentDate.getDate() ? new Date().getMonth()+1 : new Date().getMonth(),
-            1);
-        for (let index = 0; index < this.getNumberOfMissingMonth(); index++) {
+        let today = new Date();
+        while (today > this.payedUp) {
             missingMonth.push(today);
-            today = new Date(today.getTime() - this.oneMonth());
+            today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
         }
+
         return missingMonth;
     }
 
@@ -71,17 +55,18 @@ class User {
 let totalAmount = 0;
 const main = document.querySelector("main");
 for (const key in fees) {
-    const user = new User(fees[key].name, fees[key].lastPayment, fees[key].reminder);
+    const user = new User(fees[key].name, fees[key].payedUp, fees[key].reminder);
     totalAmount += user.amount();
     main.innerHTML += 
     `<section>
         <h2>${user.name}</h2>
-        <p>Payed up: <span>${user.lastPaymentDate.toLocaleDateString()}</span></p>
+        <p>Payed up: <span>${user.payedUp.toLocaleDateString()}</span></p>
         <section>
             <p>Da Pagare: <span>${user.amount()} €</span></p>
             <p>Mesi mancati(${user.getMissingMonths().length}):</p>
             <ul>
-            ${user.getMissingMonths().map(month => `<li>${month.getMonthName()} ${month.getFullYear()}</li>`).join(' ')}
+            ${user.getMissingMonths().map(month => 
+                `<li>${month.getMonthName()}</li>`).join(' ')}
             </ul>
         </section>
     </section>`;
